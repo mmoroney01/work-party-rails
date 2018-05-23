27e2f401ff8414a8a6f84493f1a89f187edf7a94
Michael Moroney <mmoroney06@gmail.com>
2018-05-23 12:49:04 -0500

    make delete party button

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,7 @@
 class IndexController < ApplicationController
   def index
+  	@parties = Party.where(user_id: current_user.id)
+
   	render 'index'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/' => 'index#index'
-  get '/party/:id' => "parties#show"
+  get '/parties/:id' => "parties#show"
   post '/parties' => "parties#create"
+  delete '/parties/:id' => "parties#delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
