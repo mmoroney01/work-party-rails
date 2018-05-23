5507b8bb4a0de20d8dfbc6a97cb9a4cb5852a1e9
Michael Moroney <mmoroney06@gmail.com>
2018-05-23 14:03:51 -0500

    user can ajaxically delete parties

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/' => 'index#index'
+  root :controller => 'index', :action => 'index'
+
+  get '/' => 'parties#index'
   get '/party/:id' => "parties#show"
+
   post '/parties' => "parties#create"
   delete '/parties/:id' => "parties#delete"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
